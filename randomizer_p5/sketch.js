f239a43f75537828e5a88c6dea28645cb8b73507
Mona <sota.kaji@sjsu.edu>
2020-10-01 14:40:18 -0700

    add

--- randomizer_p5/sketch.js
+++ randomizer_p5/sketch.js
@@ -16,13 +16,18 @@ let pokemon = [{
 }];
 
 let randomIndex;
+let animating = false;
+// let counter = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(32);
 
-  text("☟ Click to randomize ☟", 120, 300);
+  text("☟ Click to randomize ☟", 100, 300);
+
+  // setTimeout(changeBackground, 1000);
+
 
   //console.log(pokemon.length);
   // console.log(pokemon[int(random(pokemon.length))]);
@@ -33,17 +38,41 @@ function setup() {
 }
 
 function draw() {
-}
 
-function mousePressed(){
+  if(animating == true){
+    square(random(width), random(height), random(50, 200));
+  }
+}
+// 
+// function changeBackground(){
+//   if (counter <= 5){
+//     counter++;
+//     console.log(counter)
+//   background(random(255), random(255), random(255));
+//   setTimeout(changeBackground, 1000);
+// } else {
+// }
+// }
+
+function randomizer(){
+  animating = false;
 
   if (pokemon[0]){
     background(random(200, 255));
     randomIndex = int(random(pokemon.length))
-    text(pokemon[randomIndex].name, 250, 300);
+    text(`${pokemon[randomIndex].name}'s favorite color
+    is
+    ${pokemon[randomIndex].color}`, 250, 300);
     pokemon.splice(randomIndex, 1);
   } else {
     background(random(200, 255));
-    text("nothing left!", 250, 300);
+    text("nothing left!", 225, 300);
   }
+}
+
+function mousePressed(){
+  animating = true;
+  setTimeout(randomizer, 2000);
+
+
   }
