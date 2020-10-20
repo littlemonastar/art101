a92302dbf5c6731a09bf00fdf76503da9f3e8f4f
Mona <sota.kaji@sjsu.edu>
2020-10-20 15:07:08 -0700

    2

--- drawing_machine/sketch.js
+++ drawing_machine/sketch.js
@@ -1,9 +1,11 @@
 let array = [];
+let backgroundColor = 200;
+
 
 
 function setup() {
   createCanvas(400, 400);
-  background(225);
+  background(backgroundColor);
 
   strokeWeight(5);
   noFill();
@@ -14,11 +16,17 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed){
-    // stroke(map(mouseX, 0, 600, 0, 255, true))
-    // // line(width - mouseX, height - mouseY, width -pmouseX, height - pmouseY);
+    backgroundColor -= 5;
+    background(backgroundColor);
     // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
     array.push([mouseX, mouseY]);
+    beginShape();
+    for (let i = 0; i < array.length; i++){
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
+    }
+    endShape();
+
   }
 
 }
@@ -33,16 +41,24 @@ function keyTyped() {
   } else if (key === 'd'){
     background(255);
 
-beginShape();
-    for (let i = 0; i < array.length - 1; i++){
+    beginShape();
+    for (let i = 0; i < array.length; i++){
       // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curveVertex(array[i][0], array[i][1])
     }
     endShape();
 
 
+
     line(array[1][0], array[1][1], array[2][0], array[2][1]);
   }
   return false;
+
+}
+
+function mousePressed(){
+  array = [];
+  backgroundColor = 255;
+
 
 }
