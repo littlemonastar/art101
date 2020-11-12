bbb9b1e5bd83cb1fc81fa672aa38e43cd2f53649
Mona <sota.kaji@sjsu.edu>
2020-11-12 00:44:02 -0800

    game 2

---
+++ game/player.js
@@ -0,0 +1,43 @@
+class Player {
+  constructor() {
+    this.r = 60;
+    this.x = w / 2;
+    this.y = h - this.r;
+    this.speed = 2;
+    this.diretion = 'still';
+  }
+
+  display() {
+    image(playerImg,this.x, this.y, this.r, this.r);
+    // rect(this.x, this.y, this.r, this.r);
+  }
+
+  move() {
+    switch (this.direction) {
+      case 'still':
+        break;
+      case 'up':
+        if (this.y > 0){
+            this.y -= this.speed;
+        }
+        break;
+      case 'down':
+        if (this.y < h - this.r){
+        this.y += this.speed;
+      }
+        break;
+      case 'right':
+        if (this.x < w- this.r){
+        this.x += this.speed;
+      }
+        break;
+      case 'left':
+        if (this.x > 0){
+        this.x -= this.speed;
+      }
+        break;
+      default:
+        break;
+    }
+  }
+}
--- game/sketch.js
+++ game/sketch.js
@@ -1,16 +1,29 @@
 'use strict';
 
-
 let state = 'title';
 let cnv;
 let points = 0;
 let w = 600;
 let h = 600;
+let player;
+let coins = [];
+let playerImg;
+let coinImg;
+
+function preload(){
+  playerImg = loadImage('assets/gengar.png');
+  coinImg = loadImage('assets/donut.png');
+}
+
 
 function setup() {
   cnv = createCanvas(w, h);
 
   textFont('monospace');
+
+  player = new Player();
+  // coins[0] = new Coin();
+  coins.push(new Coin());
 }
 
 function draw() {
@@ -24,59 +37,112 @@ function draw() {
       level1();
       cnv.mouseClicked(level1MouseClicked);
       break;
-      case 'you win':
+    case 'you win':
       youWin();
       cnv.mouseClicked(youWinMouseClicked);
       break;
     default:
       break;
   }
-
 }
 
-
-
-function title() {
-  background(0);
-  textSize(80);
-  fill(255);
-  textAlign(CENTER);
-  text('MY GAME', w/2, h/5);
-
-  textSize(20);
-  text('click anywhere to start! :)', w/2, h/2);
-}
-
-function titleMouseClicked() {
-  console.log('canvas is clicked on title page');
-  state = 'level 1'
-}
-
-function level1() {
-  background(50, 150, 200);
-  text('click for points', w/2, h - 30);
-}
-
-function level1MouseClicked() {
-  points++;
-  console.log('points =' + points);
-
-  if (points >= 10){
-    state = 'you win';
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    player.direction = 'left'
+  } else if (keyCode == RIGHT_ARROW) {
+    player.direction = 'right'
+  } else if (keyCode == UP_ARROW) {
+    player.direction = 'up'
+  } else if (keyCode == DOWN_ARROW) {
+    player.direction = 'down'
+  } else if (key == ' '){
+    player.direction = 'still'
+    }
   }
-}
 
-function youWin(){
-  background(255, 50, 80);
-  textSize(80);
-  stroke(255);
-  text('You Win!!', w/2, h/5);
 
-  textSize(20);
-  text('click anywhere to restart!)', h/2, h/2);
-}
 
-function youWinMouseClicked(){
-  state = 'level 1';
-  points = 0;
-}
+  function title() {
+    background(0);
+    textSize(80);
+    fill(255);
+    textAlign(CENTER);
+    text('MY GAME', w / 2, h / 5);
+
+    textSize(20);
+    text('click anywhere to start! :)', w / 2, h / 2);
+  }
+
+  function titleMouseClicked() {
+    console.log('canvas is clicked on title page');
+    state = 'level 1'
+  }
+
+  function level1() {
+    background(50, 150, 200);
+    // text('click for points', w/2, h - 30);
+
+    if(random(1) <= 0.01){
+      coins.push(new Coin());
+    }
+
+    player.display();
+    player.move();
+
+    //
+    // for (let i = 0; i < coins.length; i++){
+    //   coins[i].display();
+    //   coins[i].move();
+    // }
+
+    //using forEach loop
+
+    // coins.forEach(function(coin){
+    //   coin.display();
+    //   coin.move();
+    // })
+
+
+    for (let coin of coins){
+      coin.display();
+      coin.move();
+    }
+
+
+    //check for collision
+    for (let i = coins.length - 1; i >= 0; i--){
+    if(dist(player.x, player.y, coins[i].x, coins[i].y) <= (player.r + coins[i].r) / 2){
+      points++;
+      coins.splice(i, 1);
+    } else if (coins[i].y > h){
+      coins.splice(i, 1);
+      console.log('donut is out of town');
+    }
+  }
+
+    text(`points: ${points}`, w/4, h - 30);
+  }
+
+  function level1MouseClicked() {
+    // points++;
+    // console.log('points =' + points);
+    //
+    // if (points >= 10) {
+    //   state = 'you win';
+    // }
+  }
+
+  function youWin() {
+    background(255, 50, 80);
+    textSize(80);
+    stroke(255);
+    text('You Win!!', w / 2, h / 5);
+
+    textSize(20);
+    text('click anywhere to restart!)', h / 2, h / 2);
+  }
+
+  function youWinMouseClicked() {
+    state = 'level 1';
+    points = 0;
+  }
